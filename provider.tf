a448327f39c88a8d77c4d49aea5abc2e81bccbfd
arulsamydaniel <161838766+arulsamydaniel@users.noreply.github.com>
2026-08-27 23:34:50 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -5,23 +5,24 @@ terraform {
       version = "~> 5.0"
     }
   }
- # Adding Backend as S3 for Remote State Storage
+  
+  # Adding Backend as S3 for Remote State Storage
   backend "s3" {
-    bucket = "nvdevopsprojectbucket2"
-    key    = "gopi/terraform.tfstate"
-    region = "us-east-1"   
+    bucket = "daniel-eks-terraform-state-2026"
+    key    = "eks/terraform.tfstate"
+    region = "ap-south-2"   
   }
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region = "ap-south-2"
 }
 
 locals {
-  region = "us-east-1"
-  name = "gopi_cluster"
+  region = "ap-south-2"
+  name   = "daniel_cluster"
   vpc_cidr = "10.123.0.0/16"
-  azs      = ["us-east-1a", "us-east-1b"]
+  azs      = ["ap-south-2a", "ap-south-2b"]
   public_subnets  = ["10.123.1.0/24", "10.123.2.0/24"]
   private_subnets = ["10.123.3.0/24", "10.123.4.0/24"]
   intra_subnets   = ["10.123.5.0/24", "10.123.6.0/24"]
@@ -29,5 +30,3 @@ locals {
     Example = local.name
   }
 }
-
-
