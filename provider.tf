e28fdabfaadeff944fa522c9905518b3c7009e2f
arulsamydaniel <161838766+arulsamydaniel@users.noreply.github.com>
2026-08-28 00:47:35 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -30,3 +30,16 @@ locals {
     Example = local.name
   }
 }
+data "aws_eks_cluster" "default" {
+  name = local.name
+}
+
+data "aws_eks_cluster_auth" "default" {
+  name = local.name
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.default.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.default.token
+}
