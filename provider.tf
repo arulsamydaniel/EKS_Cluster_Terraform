c3eb4c764988e2fa336de8cc277434bf07ea63df
arulsamydaniel <161838766+arulsamydaniel@users.noreply.github.com>
2026-08-28 11:11:03 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -30,16 +30,14 @@ locals {
     Example = local.name
   }
 }
-data "aws_eks_cluster" "default" {
-  name = module.eks.cluster_name # Changed from local.name to force dependency
-}
-
-data "aws_eks_cluster_auth" "default" {
-  name = module.eks.cluster_name # Changed from local.name to force dependency
-}
 
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.default.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.default.token
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name, "--region", local.region]
+  }
 }
